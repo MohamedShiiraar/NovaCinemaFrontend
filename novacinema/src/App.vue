9618f318a25c81fc0ac3c507f3a28d700f62394f
MohamedShiiraar <mso2shiiraar@gmail.com>
2024-08-27 17:14:22 +0200

    Base changes

--- novacinema/src/App.vue
+++ novacinema/src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to ="/">
+      <h1><span>Nova </span>Cinema</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view/>
+  </main>
+ 
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fire Sans',sans-serif;
+
+  &::selection {
+    background: transparentize(#42b883,0.5);
+  }
 }
 
-nav {
-  padding: 30px;
+body {
+  background: color #35495e;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+  text-decoration: none;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: white;
+    font-size: 28px;
+
+    span {
+      color: #42b883;
+    }
+  }
 }
+
 </style>
